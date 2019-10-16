ebe02a5bbbc25d08e6448eff2b405d0ca9ec7dfd
renatobellumat <31475913+renatobellumat@users.noreply.github.com>
2019-10-15 23:21:50 -0300

    refactor(dto): Add casacade in class Cervejaria

--- src/entity/cervejaria.entity.ts
+++ src/entity/cervejaria.entity.ts
@@ -11,7 +11,7 @@ export class Cervejaria extends BaseEntity {
     @Column()
     cnpj: string;
 
-    @OneToOne(type => Pessoa)
+    @OneToOne(type => Pessoa, { cascade: true })
     @JoinColumn()
     pessoa: Pessoa;
 }
