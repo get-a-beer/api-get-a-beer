3adf8d2466e23b1c7e190b85288f4170326e8b90
Matheus Barbosa <matheuskleber@live.com>
2019-11-22 10:08:57 -0200

    feat(pagamento): adicionando controller de pagamento

--- src/module/pagamento/pagamento.controller.ts
+++ src/module/pagamento/pagamento.controller.ts
@@ -1,8 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Res, Body, HttpStatus } from '@nestjs/common';
+import { BoletoReqDTO } from 'src/dto/boleto.dto';
+import { PagamentoService } from './pagamento.service';
 
   @Controller()
   export class PagamentoController {
-    constructor() {}
+    constructor(private readonly pagService: PagamentoService) {}
     
+    @Post('/boleto')
+    async createOne(@Res() res, @Body() boletoDto: BoletoReqDTO) {
+      try{
+        const data = this.pagService.boletoPayment(boletoDto)
+        res.status(HttpStatus.OK).json({data: data})
+      }
+      catch(err){
+        res.status(HttpStatus.BAD_REQUEST).send(err);
+      }
+    }
   }
   
